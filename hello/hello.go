e8bc21587c13e606198aaa5b59e0260ea5608071
VANSH AGARWAL <vanshagarwal11@gmail.com>
2025-02-07 15:16:38 +0530

    Init

---
+++ hello/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	msgs, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(msgs)
+}
